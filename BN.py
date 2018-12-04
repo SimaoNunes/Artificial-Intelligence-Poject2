0158d4579ea923f6df21e2c5d56ee389cf937215
SimaoNunes <simaspedro1@gmail.com>
2018-12-04 19:29:51 +0000

    skrt

--- BN.py
+++ BN.py
@@ -22,14 +22,8 @@ class Node():
 
 class BN():
     def __init__(self, gra, prob):
-<<<<<<< HEAD
-        self.gra = gra
-        self.prob = prob
-        pass
-=======
         self.graph = gra
         self.prob  = prob
->>>>>>> f1660cba64b32b20c07b260782091e68a17e518a
 
     def computePostProb(self, evid):
         return 0
@@ -37,40 +31,27 @@ class BN():
     def computeJointProb(self, evid):
         total = 1
         for i in range(0,len(evid)):
-<<<<<<< HEAD
             total *= self.prob[i].computeProb(evid)[evid[i]]
         return total
 
 
+# gra = [[],[],[0,1],[2],[2]]
+# p1 = Node( np.array([.001]), gra[0] ) # burglary
+# p2 = Node( np.array([.002]), gra[1] ) # earthquake
+# p3 = Node( np.array([[.001,.29],[.94,.95]]), gra[2] ) # alarm
+# p4 = Node( np.array([.05,.9]), gra[3] ) # johncalls
+# p5 = Node( np.array([.01,.7]), gra[4] ) # marycalls
+# prob = [p1,p2,p3,p4,p5]
+# gra = [[],[],[0,1],[2],[2]]
+# bn = BN(gra, prob)
 
-
-
-gra = [[],[],[0,1],[2],[2]]
-p1 = Node( np.array([.001]), gra[0] ) # burglary
-p2 = Node( np.array([.002]), gra[1] ) # earthquake
-p3 = Node( np.array([[.001,.29],[.94,.95]]), gra[2] ) # alarm
-p4 = Node( np.array([.05,.9]), gra[3] ) # johncalls
-p5 = Node( np.array([.01,.7]), gra[4] ) # marycalls
-prob = [p1,p2,p3,p4,p5]
-gra = [[],[],[0,1],[2],[2]]
-bn = BN(gra, prob)
-
-
-
-
-
-
-ev = (1,1,1,1,1)
-jp = []
-for e1 in [0,1]:
-    for e2 in [0,1]:
-        for e3 in [0,1]:
-            for e4 in [0,1]:
-                for e5 in [0,1]:
-                    jp.append(bn.computeJointProb((e1, e2, e3, e4, e5)))
-print(jp)
-print("sum joint %.3f (1)" % sum(jp))
-=======
-            total = total *  self.prob[i].computeProb(evid)[evid[i]]               
-        return total
->>>>>>> f1660cba64b32b20c07b260782091e68a17e518a
+# ev = (1,1,1,1,1)
+# jp = []
+# for e1 in [0,1]:
+#     for e2 in [0,1]:
+#         for e3 in [0,1]:
+#             for e4 in [0,1]:
+#                 for e5 in [0,1]:
+#                     jp.append(bn.computeJointProb((e1, e2, e3, e4, e5)))
+# print(jp)
+# print("sum joint %.3f (1)" % sum(jp))
