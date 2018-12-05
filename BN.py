62b567fb2eb7b6833791492274b72b9be44e8c72
SimaoNunes <simaspedro1@gmail.com>
2018-12-05 18:13:00 +0000

    Add files via upload
    simmy

--- BN.py
+++ BN.py
@@ -22,30 +22,41 @@ class Node():
 
 class BN():
     def __init__(self, gra, prob):
-<<<<<<< HEAD
-        self.gra = gra
-        self.prob = prob
-        pass
-=======
         self.graph = gra
         self.prob  = prob
->>>>>>> 0158d4579ea923f6df21e2c5d56ee389cf937215
 
     def computePostProb(self, evid):
-        evid_clean = []
-        for i in range(0,len(evid)):
-            if evid(i) == [] or evid(i) == -1:
-                evid_clean.append(0)
-            elif evid(i) == []:
-                evid_clean.append(0)
-                unknown = i
-            else:
-                evid_clean.append(1)
-        alpha = 1 / self.computeJointProb(evid_clean)
-        for i in range(0,unknown):
+
+        total          = 0
+        size           = len(evid)
+        targetID       = None
+        subTargetsID   = []
+        hiddenValuesID = [] 
+
+        for i in range(0, size):
+            e = evid[i]
+            if e == -1:
+                targetID = i
+            elif e == 1:
+                subTargetsID.append(i)
+            elif e == []:
+                hiddenValuesID.append(i)
+
+        #alpha
+        ev = ajudante(size, subTargetsID)
+        total += 1 / self.computeProb(ev)[1]
+
+        #target
+        ev = ajudante(size, [targetID])
+        total += self.computeProb(ev)[1]
+
+        #hidden values
+        for i in hiddenValuesID:
+
+        return 0
 
 
-        return evid_clean
+
         
     def computeJointProb(self, evid):
         total = 1
@@ -54,38 +65,16 @@ class BN():
         return total
 
 
-<<<<<<< HEAD
+def ajudante(size,ids):
+    ev = [0] * size
+    for id in ids:
+        ev[id] = 1
+    return ev
 
 
-
-gra = [[],[],[0,1],[2],[2]]
-p1 = Node( np.array([.001]), gra[0] ) # burglary
-p2 = Node( np.array([.002]), gra[1] ) # earthquake
-p3 = Node( np.array([[.001,.29],[.94,.95]]), gra[2] ) # alarm
-p4 = Node( np.array([.05,.9]), gra[3] ) # johncalls
-p5 = Node( np.array([.01,.7]), gra[4] ) # marycalls
-prob = [p1,p2,p3,p4,p5]
-gra = [[],[],[0,1],[2],[2]]
-bn = BN(gra, prob)
+    
 
 
-
-
-
-
-ev = (1,1,1,1,1)
-jp = []
-for e1 in [0,1]:
-    for e2 in [0,1]:
-        for e3 in [0,1]:
-            for e4 in [0,1]:
-                for e5 in [0,1]:
-                    jp.append(bn.computeJointProb((e1, e2, e3, e4, e5)))
-print(jp)
-print("sum joint %.3f (1)" % sum(jp))
-
-print(bn.computePostProb((1,1,[],[],-1)))
-=======
 # gra = [[],[],[0,1],[2],[2]]
 # p1 = Node( np.array([.001]), gra[0] ) # burglary
 # p2 = Node( np.array([.002]), gra[1] ) # earthquake
@@ -106,4 +95,3 @@ print(bn.computePostProb((1,1,[],[],-1)))
 #                     jp.append(bn.computeJointProb((e1, e2, e3, e4, e5)))
 # print(jp)
 # print("sum joint %.3f (1)" % sum(jp))
->>>>>>> 0158d4579ea923f6df21e2c5d56ee389cf937215
